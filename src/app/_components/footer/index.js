4ef395e3ee5293a3ead9c85a490eb60564778ee3
Arjun Poudel <arjunpoudel703@gmail.com>
2025-02-18 05:52:49 +0545

    ..

--- src/app/_components/footer/index.js
+++ src/app/_components/footer/index.js
@@ -4,23 +4,34 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <Box sx={{ textAlign: 'center', py: 5 }}>
-      <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-        <Link href="/feed" style={linkStyle}><Typography variant="body2">Home</Typography></Link>
-        <Link href="/pages/about-us" style={linkStyle}><Typography variant="body2">About us</Typography></Link>
-        <Link href="/pages/contact-us" style={linkStyle}><Typography variant="body2">Contact us</Typography></Link>
-        <Link href="/pages/be-the-one" style={linkStyle}><Typography variant="body2">Be the One</Typography></Link>
-        <Link href="/pages/privacy-policy" style={linkStyle}><Typography variant="body2">Privacy Policy</Typography></Link>
-        <Link href="/pages/terms-of-use" style={linkStyle}><Typography variant="body2">Terms of use</Typography></Link>
+    <div style={divStyle}>
+      <Box sx={{ textAlign: 'center'}}>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+          <Link href="/feed" style={linkStyle}><Typography variant="body2">Home</Typography></Link>
+          <Link href="/pages/about-us" style={linkStyle}><Typography variant="body2">About us</Typography></Link>
+          <Link href="/pages/contact-us" style={linkStyle}><Typography variant="body2">Contact us</Typography></Link>
+          <Link href="/pages/be-the-one" style={linkStyle}><Typography variant="body2">Be the One</Typography></Link>
+          <Link href="/pages/privacy-policy" style={linkStyle}><Typography variant="body2">Privacy Policy</Typography></Link>
+          <Link href="/pages/terms-of-use" style={linkStyle}><Typography variant="body2">Terms of use</Typography></Link>
+        </Box>
       </Box>
-    </Box>
+    </div>
   );
 };
 
 const linkStyle = {
   textDecoration: 'none',
   margin: '5px 15px', // Slight margin adjustment for better spacing
-  color: 'inherit', // Ensure text color is consistent with parent
+  color: 'inherit', // Ensure text color is consistent with parent.
 };
+
+
+const divStyle = {
+  background: 'white',
+  position:'fixed',
+  bottom: '0px',
+  padding: '5px 0',
+  width: '100%'
+}
 
 export default Footer;
