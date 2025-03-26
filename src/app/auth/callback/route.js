0cece7a0340783211b9e920f7103bbef83aa2098
Arjun Poudel <arjunpoudel479@gmail.com>
2025-03-26 07:34:29 +0545

    ..

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -1,11 +1,17 @@
+import jwt from 'jsonwebtoken'
+
 export async function POST(request) {
 
-    console.log("from callback")
-    console.log(request)
 
     const rawBody = await request.text(); // Read the request body as a text string
-    console.log("Raw Body:", rawBody);
 
+    const params = new URLSearchParams(rawBody);
+
+    const id_token = params.get('id_token');
+
+    const decodedJWT = jwt.decode(id_token);
+
+    console.log(decodedJWT)
 
     return new Response(null, {
         status: 302,
