0303b1d60d9e297d8313d79502e64cd42c007d89
Arjun Poudel <arjunpoudel479@gmail.com>
2025-03-26 17:39:46 +0545

    ..

--- src/app/pages/be-the-one/page.js
+++ src/app/pages/be-the-one/page.js
@@ -15,50 +15,64 @@ export default function DashboardLayout({params,  children }) {
       }, [])
 
     return(
-        <>
+        <div style={{position:'relative', paddingBottom:'20px', color:'white'}}>
           <Header />
-          <Container>
-          <h1>Be the One</h1>
-    <div>
-        <p>That Appreciates the Arts</p>
-        <p>Helping Positive Artists Here</p>
-        <p>And the Creativity it Leads to</p>
-        <p>Positive Expressions that:</p>
-        <ul>
-            <li>Inspire</li>
-            <li>Encourage</li>
-            <li>Empower</li>
-            <li>Intrigue</li>
-            <li>Entertain</li>
-        </ul>
-        <p>Artists and Viewers:</p>
-        <p>Share Your Positive Thoughts</p>
-        <ul>
-            <li>Gifts</li>
-            <li>Care</li>
-            <li>Love</li>
-        </ul>
-        <p>Take Your Time</p>
-        <p>Browse the Positive:</p>
-        <ul>
-            <li>Visual Art</li>
-            <li>Audio Art</li>
-            <li>Written Word</li>
-        </ul>
-        <p>And Empathetically Embrace What We Share</p>
-        <p>If We Positively Move You:</p>
-        <ul>
-            <li>Bring Beauty to Your World</li>
-            <li>Share that Beauty</li>
-            <li>Leave a Positive Comment</li>
-            <li>A Monetary Gift to Help</li>
-        </ul>
-        <p>Inspire, Encourage, and Empower Positive Artists!</p>
-    </div>
-           
+          <img style={{position:'absolute',  top:'0', width: '100%', minHeight:'100vh', height:'100%', objectFit:'cover'}} src="/site-assets/be-the-one-bg.jpg" />
+
+          <div style={{position:'relative', textShadow:'2px 2px 4px rgba(0, 0, 0, 0.75)'}}>  
+
+          <Container maxWidth={'md'} >
+            <h1>Be the One</h1>
+
+          <Grid container spacing={1}>
+            <Grid item xs={12} md={6}>
+                <div>
+                    <p>That Appreciates the Arts</p>
+                    <p>Helping Positive Artists Here</p>
+                    <p>And the Creativity it Leads to</p>
+                    <p>Positive Expressions that:</p>
+                    <ul>
+                        <li>Inspire</li>
+                        <li>Encourage</li>
+                        <li>Empower</li>
+                        <li>Intrigue</li>
+                        <li>Entertain</li>
+                    </ul>
+                    <p>Artists and Viewers:</p>
+                    <p>Share Your Positive Thoughts</p>
+                    <ul>
+                        <li>Gifts</li>
+                        <li>Care</li>
+                        <li>Love</li>
+                    </ul>
+                </div>
+
+            </Grid>
+            <Grid item xs={12} md={6}>
+            <div>
+                    <p>Take Your Time</p>
+                    <p>Browse the Positive:</p>
+                    <ul>
+                        <li>Visual Art</li>
+                        <li>Audio Art</li>
+                        <li>Written Word</li>
+                    </ul>
+                    <p>And Empathetically Embrace What We Share</p>
+                    <p>If We Positively Move You:</p>
+                    <ul>
+                        <li>Bring Beauty to Your World</li>
+                        <li>Share that Beauty</li>
+                        <li>Leave a Positive Comment</li>
+                        <li>A Monetary Gift to Help</li>
+                    </ul>
+                    <p>Inspire, Encourage, and Empower Positive Artists!</p>
+                </div>
+            </Grid>
+        </Grid>  
 
           </Container>
-        </>
+          </div>
+        </div>
       )
 
 }
