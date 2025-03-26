2261ae64564dcb1b2b9091e71098c033c9e803de
Arjun Poudel <arjunpoudel479@gmail.com>
2025-03-26 07:28:18 +0545

    ..

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -2,6 +2,11 @@ export async function POST(request) {
 
     console.log("from callback")
     console.log(request)
+
+    const rawBody = await request.text(); // Read the request body as a text string
+    console.log("Raw Body:", rawBody);
+
+
     return new Response(null, {
         status: 302,
         headers: {
