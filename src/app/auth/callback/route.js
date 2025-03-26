0a773efb5816eb6b8af65876a1516b40799881ba
Arjun Poudel <arjunpoudel479@gmail.com>
2025-03-26 07:22:22 +0545

    ..

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -1,5 +1,7 @@
-export async function POST() {
+export async function POST(request) {
+
     console.log("from callback")
+    console.log(request)
     return new Response(null, {
         status: 302,
         headers: {
